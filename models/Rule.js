8dd8e9ab5d266e46f8d42f5efb15c5594cd1d576
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-04-08 22:27:30 -0300

    feat: add all files

--- models/Rule.js
+++
@@ -1,9 +0,0 @@
-import { model, Schema, models } from "mongoose"
-
-const RuleSchema = new Schema ({
-    name: { type: String, lowercase: true, required: true },
-}, {
-    timestamps: true,
-});
-
-export const Rule = models.Rule || model('Rule', RuleSchema);
